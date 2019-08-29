ccdfc1b81545b0fc4fb7d25b4e3b92cc457151ed
mabhay1 <mabhay1994@gmail.com>
2019-08-29 18:44:13 +0530

    removed a comment

--- hmsapp/admin.py
+++ hmsapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from.models import UserProfile, UserHistory,Medic,Labs,Visits,Case
-# Register your models here.
+# Register your models here
 admin.site.register(UserProfile)
 admin.site.register(UserHistory)
 admin.site.register(Medic)
